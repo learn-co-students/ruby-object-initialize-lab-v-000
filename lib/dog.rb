163d2419757b004406038c3c32422da661110285
phamducv <phamducv@gmail.com>
2016-01-05 00:51:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+
+        def initialize(name, breed="Mut")
+                @breed=breed
+                @name=name
+        end
+        def breed
+                @breed=breed
+        end
+        def breed
+                @breed
+
+        end
+
+
+
+
+
+
+
+
+end
