59d41defc65e71e393a386c914b083e772a0316f
PeteAJ <peter.jacobson@gmail.com>
2016-02-25 12:43:11 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,6 @@
 class Dog
-   def initialize(name, breed)
+  def initialize(name, breed = "Mut")
     @name = name
-    @breed = "Mut"
+    @breed = breed
   end
- 
-expect(fido.instance_variable_get(:@breed)).to eq("Mut")
 end
- 
-  def id=(id_string)
-    # this method will now take in a string that contains
-    # a first name and a last name, separated by a space,
-    # like this: "Kanye West"
- 
-    @name = id_string.split(" ").name
-    @breed = id_string.split(" ").breed
-  end
- 
-  def id
-    "#{@name} #{@breed}"
-  end
-
-
-end
-
-fido = Dog.new("Fido", " ")
-fido.id
