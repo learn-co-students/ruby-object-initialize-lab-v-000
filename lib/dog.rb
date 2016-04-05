575a69a1306f1da4851fe84f54464b1888c0a915
SnowboardTechie <bryan.thompson518@gmail.com>
2016-04-04 18:05:00 -0600

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+  def initialize(name_given, breed_given = "Mut")
+    @name = name_given
+    @breed = breed_given
+  end
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(name_given)
+    @name = name_given
+  end
+
+  def name=(name_given)
+    @name = name_given
+  end
+
+  def name
+    @name
+  end
+end
