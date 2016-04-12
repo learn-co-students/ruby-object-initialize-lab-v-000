73c7be88918364cf858079b3ed77cc7667140486
afordfa <aggiemis@yahoo.com>
2016-04-12 00:27:01 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+
+  def initialize(name, breed="Mut")
+    @breed = breed
+    @name = name
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+end
