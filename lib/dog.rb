e6a33f1909fda5491e16fe17985dedd924ca0bf5
Tom Maddox <tmaddoxl@gmail.com>
2016-05-17 21:27:14 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,6 @@
 class Dog
- def initialize(name, breed)
+ def initialize(name, breed = "Mut")
   @name = name
   @breed = breed
  end
-
- def name=(name_breed)
-  @name = name
-  @breed = breed
- end
-
- def name
-   "#{@name}, #{@breed}"
-   @breed
- end
-
-fido = Dog.new("Fido","Pug")
-fido.name
+end
