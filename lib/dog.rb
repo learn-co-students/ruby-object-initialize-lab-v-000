602145011c2a486676dc1976e2be08071c7ee1ff
Bob Habig <rfhab2@gmail.com>
2016-05-20 13:14:27 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,15 @@
 class Dog
   def initialize(name, breed = "Mut")
-    @name = name
-    @breed = breed
+    @name=name
+    @breed=breed
   end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+
 end
-
-
--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,9 @@
 class Person
   def initialize(name)
-    @name = name
+    @name=name
+  end
+
+  def name
+    @name
   end
 end
