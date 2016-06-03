e451ff54c7d009f2035cb9c08c646404d8272750
Jason Casares <jasonc@csdrafting.com>
2016-06-03 14:22:38 -0700

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  attr_accessor :name, :breed
+  def initialize(name, breed="Mut")
+    @name = name
+    @breed = breed
+  end
+
+
+    #if breed.empty? || breed.nil?
+    #breed = "Mut"
+  #end
+
+end
