a66efdfcc70f82731117a76798ed52a8f944fb26
ga-be <gj@mail.co.de>
2016-06-16 01:05:20 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+  def initialize(init_name, init_breed = "Mut")
+    @name = init_name
+    @breed = init_breed
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,5 @@
+class Person
+  def initialize(init_name)
+    @name = init_name
+  end
+end
