da13ecbdd6e5ecb0734edc847342078703012d09
Brooke Bowles <brookebowles@me.com>
2016-10-20 13:45:39 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,27 +1,8 @@
 class Dog
+  attr_accessor :name, :breed
+
   def initialize(name, breed = "Mutt")
     @name = name
     @breed = breed
-
   end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-
-  def breed
-    @breed
-  end
-
-  def breed=(new_breed)
-    @breed = new_breed
-  end
- end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,7 @@
 class Person
+attr_accessor :name
+
   def initialize(name)
     @name = name
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def name
-    @name
   end
 end
