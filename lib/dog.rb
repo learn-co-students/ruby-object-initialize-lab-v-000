774768b673dfe36d34b2fbe2c938eaf285c1c713
nathalie garcia <nathalie.scruz@gmail.com>
2017-05-20 01:51:46 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  def name_breed=(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+  def name_breed(name, breed="Mutt")
+    @name
+    @breed
+  end
+end
