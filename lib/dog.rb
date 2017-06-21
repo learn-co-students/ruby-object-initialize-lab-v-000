0ef65a6153ca768064556275a527ab1d8fe61011
Adam Ping <thepingster@gmail.com>
2017-06-21 14:50:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+  attr_reader :name , :breed
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+  def name=(name)
+    @name = name
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+end
