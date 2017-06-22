7ae4822d1f535f078e37db8ac4d018cfa8807d7b
Auden Robertson <audenrobertson@gmail.com>
2017-06-22 06:49:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def initialize(dog_name, dog_breed="Mutt")
+    @name = dog_name
+    @breed = dog_breed
+    end
+
+  def name_breed=(dog_name, dog_breed)
+    @name = dog_name
+    @breed = dog_breed
+  end
+
+  def name_breed
+    @name
+    @breed
+  end
+
+end
