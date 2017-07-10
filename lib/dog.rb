c9c36a1b5f0ea47a9f9a618b9689fa9ab54ffd34
Jen Poole <contact.vandepool@gmail.com>
2017-07-10 05:09:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,6 @@
 class Dog
-
-    def initialize(name, breed='Mut')
-        @name = name
-      @breed = breed
-    end
-
+  def initialize(name, breed='Mutt')
+    @name = name
+    @breed = breed
+  end
 end
-
--- spec/object_initialize_spec.rb
+++ spec/object_initialize_spec.rb
@@ -25,7 +25,7 @@ describe 'Dog' do
     it 'defaults the breed argument to "Mut" in an instance variable @breed' do
       fido = Dog.new("Fido")
 
-      expect(fido.instance_variable_get(:@breed)).to eq("Mut")
+      expect(fido.instance_variable_get(:@breed)).to eq("Mutt")
     end
   end
 end
