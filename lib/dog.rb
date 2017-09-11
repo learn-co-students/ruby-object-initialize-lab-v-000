6ffebb6b8859754dd0af0f3716f3534ec194b838
smithwebtek <brad@smithwebtek.com>
2017-09-11 15:45:08 -0400

    fixed name= and breed=

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,23 @@
 class Dog
 
-  def initialize(name_of_dog, breed_of_dog = "Mut")
+  def initialize(name_of_dog = "noname", breed_of_dog = "Mut")
     @name = name_of_dog
     @breed = breed_of_dog
   end
 
   def name=(name_of_dog)
-    @name
-    end
+    @name = name_of_dog
+  end
 
   def name
     @name
   end
 
   def breed=(breed_of_dog)
-    @breed
+    @breed = bread_of_dog
   end
   
   def breed
     @breed
-  end
-  
+	end
 end
