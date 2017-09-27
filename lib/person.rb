c1177e2b5e8d33c4055aaf78be2b1743d6f32d2c
Lawrence Savidge <savidge00@gmail.com>
2017-09-27 19:43:21 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -3,12 +3,12 @@ class Person
     @name = name
   end
 
-  def name=(name)
-    @name = name
-  end
+#  def name=(name)
+#    @name = name
+#  end
 
-  def name
-    @name
-  end
+#  def name
+#    @name
+#  end
 
 end
