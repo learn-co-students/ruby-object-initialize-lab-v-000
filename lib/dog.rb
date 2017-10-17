4c9cadfcbae8075680ecc72f89a4fe19a5480c20
Elle Faraday <faraday.elle@gmail.com>
2017-10-17 02:02:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def breed
+    breed = @breed
+  end
+end
+
+
+fido = Dog.new(fido)
+
+puts fido.breed
