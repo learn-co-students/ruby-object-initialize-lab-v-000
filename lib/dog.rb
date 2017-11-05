1f9a64b76c3e1cfec0a96f09851d5dbffcd98798
Eric Johnson <0216johnsoneric@gmail.com>
2017-11-05 03:12:02 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name
+    @name
+  end
+
+  def breed
+    @breed
+  end
+end
+
+# name = eric
+#   def name=(new_name)
+#     @name = new_name
+#   end
+#
+# end
