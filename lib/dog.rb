a79f95880623dcd694840c0d196ee938913c70ee
Petya Djanovska <petya.djanovska@gmail.com>
2017-11-27 16:16:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,6 @@
+class Dog
+def initialize (inst_name,inst_breed = "Mutt")
+  @name = inst_name
+  @breed = inst_breed
+end
+end
---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+def initialize(inst_name)
+  @name = inst_name
+end
+
+def name= (inst_name)
+  @name = inst_name
+end
+
+def name
+  @name
+end
+end
