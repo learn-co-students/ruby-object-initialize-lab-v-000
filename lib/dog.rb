cd44d48023f616afbef7d1a20b9e01e7a9970056
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 20:54:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,16 @@
 class Dog
   attr_accessor :name, :breed
   attr_reader :name
+
+  def initialize(name, breed = "Mutt")
+    @name = name
+    @breed = breed
+  end
 end
 
+
+# class Dog
+#
 #   def initialize(name, breed = "Mutt")
 #     @name = name
 #     @breed = breed
