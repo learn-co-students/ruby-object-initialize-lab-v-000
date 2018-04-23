47782b3caecc0bd48eb313f79b7cb5702ff0aedc
Mehwish Aziz <mehwisha@msn.com>
2018-04-23 02:26:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+
+def initialize(name, breed = "Mutt")
+@name = name
+@breed = breed
+end
+# def breed=(new_breed)
+#     @breed = breed
+#   end
+#
+#   def breed
+#     @breed = breed
+#   end
+end
+# fido = Dog.new
+# fido.breed = "Mutt"
