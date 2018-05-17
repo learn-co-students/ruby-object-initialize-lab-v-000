8ca3d075cdbc9f6a63f7b212099ef521ec294430
Carrie Savinell <carrielane89@gmail.com>
2018-05-17 13:58:37 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,14 @@
-class Person
- def initialize(name) 
-   @name = name 
- end    
-end
+class Person 
+  def initialize(name)
+    @name = name 
+  end   
+  
+#  def name= (name_of_person)
+#    @name = name_of_person
+#  end 
+  
+#  def name 
+#    @name 
+#  end 
+  
+end   
