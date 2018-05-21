1f0eb5898041906496e6e8f881d4943b9075d12c
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-21 04:42:45 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def initialize(name = "Mutt", breed = "Mutt")
+    @name = name
+    @breed = breed
+end
+
+  def dog=(name = "Mutt", breed = "Mutt")
+    @name = name
+    @breed = breed
+end
+  def dog
+    @name
+    @breed
+  end
+end
