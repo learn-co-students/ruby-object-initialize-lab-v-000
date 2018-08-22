8834a6ee0852a834ec11e91262639207bc304755
Liebe Brand <liebebrand@gmail.com>
2018-08-22 14:45:31 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,20 @@
+
+class Person
+
+  def initialize(name) # initialize the method
+    @name = name
+  end
+
+  def name=(name) # writing the method
+    @name = name
+  end
+
+  def name # reading the method
+    @name
+  end
+
+end
+
+# beyonce = Person.new("Beyonce")
+
+# puts beyonce.name
