88bf3bfa057490e44c40ece8f722985d2de28516
Rachel Wyatt <rw126@evansville.edu>
2018-08-24 23:59:40 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+
+  def person_name=(person_name)
+    @name = person_name
+  end
+
+  def name
+    @name
+  end
+
+end
+
+
+
+#  class Dog
+#  def initialize(breed)
+#    @breed = breed
+#  end
+
+#  def breed=(breed)
+#    @breed = breed
+#  end
+
+#  def breed
+#    @breed
+#  end
+#nd
