df349b6a7003466dcd8e6c7e005987edfd7f68b7
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2018-09-01 18:42:35 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Person
+  def initialize(the_persons_name)
+    @name = the_persons_name
+  end
+
+  def name=(the_persons_name)
+    @name = the_persons_name
+  end
+
+  def name
+    @name
+  end
+end
