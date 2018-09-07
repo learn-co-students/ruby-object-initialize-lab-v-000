cf97fdb3e2ed097ce30b3828b727f0e1cb8a8642
Sherry Martin <sm674@cornell.edu>
2018-09-07 19:18:52 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,29 +1,9 @@
 class Dog
+  attr_accessor :name, :breed
+  def initialize(name, breed)
+   @name = name
+   @breed = breed
+  end
 end
-Mutt = Dog.new
 
-class Dog
-  def initialize(name)
-    @name = name
-  end
- 
-  def name=(name)
-    @name = name
-  end
- 
-  def name
-    @name
-  end
-  
-  def initialize(breed)
-    @breed = breed
-  end
-  
-  def breed= (breed)
-    @breed = breed 
-  end
-  
-  def breed 
-    @breed 
-  end
-end
+
