20ddd09981fa10e4d7118ee394e28cacaf6b9819
Sherry Martin <sm674@cornell.edu>
2018-09-07 18:26:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,19 @@
 class Dog
-  def initialize(breed)
-    @breed = breed
-  end
- 
-  def breed=(breed)
-    @breed = breed
-  end
- 
-  def breed
-    @breed
-  end
 end
+Mutt = Dog.new
+
+class Dog
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
+  end
   
   def initialize(breed)
     @breed = breed
