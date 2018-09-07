b6e46fa23cef0ca2a6a7274bf66f67876e15eff3
Sherry Martin <sm674@cornell.edu>
2018-09-07 18:18:24 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,26 @@
+class Dog
+  def initialize(breed)
+    @breed = breed
+  end
+ 
+  def breed=(breed)
+    @breed = breed
+  end
+ 
+  def breed
+    @breed
+  end
+end
+  
+  def initialize(breed)
+    @breed = breed
+  end
+  
+  def breed= (breed)
+    @breed = breed 
+  end
+  
+  def breed 
+    @breed 
+  end
+end
