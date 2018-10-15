236a106cc93010714a8bb59b9cab0e3629db1472
Elias Ventura <eliasventura23@gmail.com>
2018-10-15 17:36:02 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,6 @@
 class Dog
-<<<<<<< HEAD
-
-  def initialize(name, breed= "Mut")
-    @breed= breed
-    @name= name
-=======
   def initialize(name, breed = "Mut")
     @name = name
     @breed = breed
->>>>>>> 890a1c224614b59f9a79d13de6e942eba4e06a10
   end
 end
--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,6 @@
 class Person
 
   def initialize(name)
-    @name= name
+    @name = name
   end
-<<<<<<< HEAD
-=======
-
->>>>>>> 890a1c224614b59f9a79d13de6e942eba4e06a10
 end
