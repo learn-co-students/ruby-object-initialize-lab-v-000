aaf1bb4ea2fc1e2bed365b0cb6ef8e39e2d9333f
Kari O'Neal <triadfirewood@gmail.com>
2019-02-19 19:02:18 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,16 +2,6 @@ class Dog
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
-end
-
-  def name=(name_breed)
-    name,breed=name_breed.split
-    @name = name
-    @breed = breed
-  end
-  
-  def name
-    "#{@name} #{@breed}".strip
   end
 end
 
